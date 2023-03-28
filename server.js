1daffc85d2970d6066668f114fcb9b2a7d3d8627
Andreas L <114237284+Sinnfull85@users.noreply.github.com>
2023-03-28 12:44:49 +0200

    update + rename any .js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const router = require("./router")
+const router = require("./usersRouter")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
