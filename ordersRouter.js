9e5e11281cef5d15cf250d2ae5784613d7410902
Andreas L <114237284+Sinnfull85@users.noreply.github.com>
2023-03-28 13:29:17 +0200

    update ordersRouter

--- ordersRouter.js
+++ ordersRouter.js
@@ -3,7 +3,7 @@ const pool = require("./databank");
 
 const router2 = Router();
 
-router.get("/user", async (req, res) => {
+router2.get("/user", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * from users");
     res.json({ data: rows });
@@ -12,7 +12,7 @@ router.get("/user", async (req, res) => {
   }
 });
 
-router.get("/user/:id", async (req, res) => {
+router2.get("/user/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const { rows } = await pool.query("SELECT * FROM users WHERE id=$1;", [id]);
@@ -22,7 +22,7 @@ router.get("/user/:id", async (req, res) => {
   }
 });
 
-router.post("/user", async (req, res) => {
+router2.post("/user", async (req, res) => {
   const { firstname, lastname, age } = req.body;
   try {
     const { rows } = await pool.query(
