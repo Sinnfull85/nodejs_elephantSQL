eddcf828f35a278b759b837d8d412bf3b09abaf0
Andreas L <114237284+Sinnfull85@users.noreply.github.com>
2023-03-28 12:32:52 +0200

    update server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const router = require("./router")
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.send('Haaallllloooo Welt!!!')
+})
+
+app.use("/api", router)
+
+module.exports = app;
+
